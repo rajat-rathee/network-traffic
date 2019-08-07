bc4cd2280f3b4f29616de22c947304d33cca0e30
Rajat <>
2019-08-07 12:23:04 -0700

    added templates to insert html code

--- application/__init__.py
+++ application/__init__.py
@@ -2,11 +2,12 @@ __all__ = ['fstore', 'netscan','scheduler']
 import fstore
 import netscan
 import scheduler
-from flask import Flask, g
+from flask import Flask, g, render_template
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Currently " + str(netscan.count()) + " people are on your network."
+	num_people = netscan.count()
+	return render_template('index.html', var = num_people)
