fd550f6b5e89d8fd6445ab4a6969aa5a37b1abc7
Rajat Rathee <rrathee2@illinois.edu>
2020-08-07 22:09:59 -0700

    Update netscan.py

--- application/netscan.py
+++ application/netscan.py
@@ -8,15 +8,15 @@ def scan_network():
 	sudo_password = 'placeholder' #sudo password here
 	command = '-'
 	command = command.split()
-	cmd1 = subprocess.Popen(['echo',sudo_password], stdout=subprocess.PIPE)
-	cmd2 = subprocess.Popen(['sudo','-s'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
+# 	cmd1 = subprocess.Popen(['echo',sudo_password], stdout=subprocess.PIPE)
+# 	cmd2 = subprocess.Popen(['sudo','-s'] + command, stdin=cmd1.stdout, stdout=subprocess.PIPE)
 
 	current_time = time.time()
 
 	command = ['sudo', 'arp-scan' ,'-l','-r','5']
 	manu_list = []
 	users = []
-	valid_mac = ['1c:1a:c0:8f:45:88', '3c:ab:8e:d2:63:58', '28:e0:2c:6d:b2:46', 'ac:63:be:c0:a1:d1']
+	valid_mac = ['29:4F:BC:5A:AF:42', '76:B9:EC:BD:84:43', '6B:FC:20:D4:91:44', 'E9:F8:83:AF:95:87']
 	valid_names = ['John', 'Jane', 'Mary', 'Andy']
 
 	out = subprocess.check_output(command).decode('utf-8')
